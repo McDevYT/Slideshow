19582771fbd9ce4b05e0c40c043393c025366fc4
Gabriel Hollenstein <gabriel.hollenstein@vertigis.com>
2025-11-10 11:10:10 +0100

    wrong upload path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = path.join(__dirname, "public/images");
+const uploadDir = path.join(__dirname, "../public/images");
 fs.mkdirSync(uploadDir, { recursive: true });
 
 const app = express();
